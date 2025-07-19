69203a714a731039984518b6b28639984a444570
podhmo <ababjam61+github@gmail.com>
2025-07-19 23:32:05 +0900

    Fix/gen test work around (#53)
    * test: fix test (remove GOPATH)
    
    * test: fix tests

--- cmd/veritas/gen/analyzer_test.go
+++ cmd/veritas/gen/analyzer_test.go
@@ -1,13 +1,21 @@
 package gen_test
 
 import (
+	"bytes"
 	"flag"
+	"fmt"
+	"go/types"
+	"io"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
-	"github.com/gostaticanalysis/codegen/codegentest"
 	"github.com/podhmo/veritas/cmd/veritas/gen"
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/analysistest"
+	"golang.org/x/tools/go/analysis/checker"
+	"golang.org/x/tools/go/packages"
 )
 
 var flagUpdate bool
@@ -19,22 +27,68 @@ func TestMain(m *testing.M) {
 }
 
 func TestGenerator(t *testing.T) {
-	wd, err := os.Getwd()
-	if err != nil {
-		t.Fatalf("failed to get current directory: %v", err)
+	dir := analysistest.TestData() + "/src"
+
+	goldenFile := filepath.Join(dir, "a/gogen.golden")
+
+	// This is a workaround for the fact that codegentest is broken.
+	buf := new(bytes.Buffer)
+	gen.Generator.Output = func(pkg *types.Package) io.Writer {
+		return buf
 	}
-	testdata := filepath.Join(wd, "testdata")
-	t.Setenv("GOPATH", testdata)
-	rs := codegentest.Run(t, testdata, gen.Generator, "a")
-	if len(rs) > 0 {
-		for _, r := range rs {
-			if r.Err != nil {
-				t.Errorf("unexpected error: %v", r.Err)
-			}
-			if r.Output != nil {
-				t.Log(r.Output.String())
-			}
+	an := gen.Generator.ToAnalyzer()
+
+	_, err := doGenerate(dir, an, []string{"./a"})
+	if err != nil {
+		t.Fatalf("failed to generate code: %v", err)
+	}
+
+	if flagUpdate { // update golden file if -update flag is set
+		if err := os.WriteFile(goldenFile, buf.Bytes(), 0644); err != nil {
+			t.Fatalf("failed to write golden file: %v", err)
+		}
+	} else {
+		golden, err := os.ReadFile(goldenFile)
+		if err != nil {
+			t.Fatalf("failed to read golden file: %v", err)
+		}
+		want, got := strings.TrimSpace(string(golden)), strings.TrimSpace(buf.String())
+		if want != got {
+			t.Errorf("output does not match golden file:\nwant:\n%s\ngot:\n%s", want, got)
 		}
 	}
-	codegentest.Golden(t, rs, flagUpdate)
+}
+
+// codegentest is broken, so we use individual code.
+func doGenerate(dir string, an *analysis.Analyzer, patterns []string) (*checker.Graph, error) {
+	mode := packages.NeedName | packages.NeedFiles | packages.NeedCompiledGoFiles | packages.NeedImports |
+		packages.NeedTypes | packages.NeedTypesSizes | packages.NeedSyntax | packages.NeedTypesInfo |
+		packages.NeedDeps | packages.NeedModule
+	cfg := &packages.Config{
+		Mode:  mode,
+		Dir:   dir,
+		Tests: true,
+		Env:   os.Environ(),
+	}
+	pkgs, err := packages.Load(cfg, patterns...)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load packages: %w", err)
+	}
+
+	for _, pkg := range pkgs {
+		if pkg.Name == "" {
+			return nil, fmt.Errorf("failed to load %q: Errors=%v", pkg.PkgPath, pkg.Errors)
+		}
+	}
+
+	if len(pkgs) == 0 {
+		return nil, fmt.Errorf("no packages matched %s", patterns)
+	}
+
+	res, err := checker.Analyze([]*analysis.Analyzer{an}, pkgs, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to analyze packages: %w", err)
+	}
+
+	return res, nil
 }
