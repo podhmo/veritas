7968212547eb4e3f8b0caaa9833a77a8fbf0e254
podhmo <ababjam61+github@gmail.com>
2025-07-19 23:44:35 +0900

    test: refactor tests

--- cmd/veritas/gen/analyzer_test.go
+++ cmd/veritas/gen/analyzer_test.go
@@ -27,22 +27,21 @@ func TestMain(m *testing.M) {
 }
 
 func TestGenerator(t *testing.T) {
-	dir := analysistest.TestData() + "/src"
-
-	goldenFile := filepath.Join(dir, "a/gogen.golden")
-
 	// This is a workaround for the fact that codegentest is broken.
 	buf := new(bytes.Buffer)
 	gen.Generator.Output = func(pkg *types.Package) io.Writer {
 		return buf
 	}
-	an := gen.Generator.ToAnalyzer()
 
-	_, err := doGenerate(dir, an, []string{"./a"})
-	if err != nil {
+	if err := doGenerate(
+		analysistest.TestData()+"/src",
+		gen.Generator.ToAnalyzer(),
+		[]string{"./a"},
+	); err != nil {
 		t.Fatalf("failed to generate code: %v", err)
 	}
 
+	goldenFile := filepath.Join(analysistest.TestData(), "src/a/gogen.golden")
 	if flagUpdate { // update golden file if -update flag is set
 		if err := os.WriteFile(goldenFile, buf.Bytes(), 0644); err != nil {
 			t.Fatalf("failed to write golden file: %v", err)
@@ -60,7 +59,7 @@ func TestGenerator(t *testing.T) {
 }
 
 // codegentest is broken, so we use individual code.
-func doGenerate(dir string, an *analysis.Analyzer, patterns []string) (*checker.Graph, error) {
+func doGenerate(dir string, an *analysis.Analyzer, patterns []string) error {
 	mode := packages.NeedName | packages.NeedFiles | packages.NeedCompiledGoFiles | packages.NeedImports |
 		packages.NeedTypes | packages.NeedTypesSizes | packages.NeedSyntax | packages.NeedTypesInfo |
 		packages.NeedDeps | packages.NeedModule
@@ -72,23 +71,23 @@ func doGenerate(dir string, an *analysis.Analyzer, patterns []string) (*checker.
 	}
 	pkgs, err := packages.Load(cfg, patterns...)
 	if err != nil {
-		return nil, fmt.Errorf("failed to load packages: %w", err)
+		return fmt.Errorf("failed to load packages: %w", err)
 	}
 
 	for _, pkg := range pkgs {
 		if pkg.Name == "" {
-			return nil, fmt.Errorf("failed to load %q: Errors=%v", pkg.PkgPath, pkg.Errors)
+			return fmt.Errorf("failed to load %q: Errors=%v", pkg.PkgPath, pkg.Errors)
 		}
 	}
 
 	if len(pkgs) == 0 {
-		return nil, fmt.Errorf("no packages matched %s", patterns)
+		return fmt.Errorf("no packages matched %s", patterns)
 	}
 
-	res, err := checker.Analyze([]*analysis.Analyzer{an}, pkgs, nil)
+	_, err = checker.Analyze([]*analysis.Analyzer{an}, pkgs, nil)
 	if err != nil {
-		return nil, fmt.Errorf("failed to analyze packages: %w", err)
+		return fmt.Errorf("failed to analyze packages: %w", err)
 	}
 
-	return res, nil
+	return nil
 }
